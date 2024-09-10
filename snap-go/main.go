b6f4ba5dba43265bf44bce010197d23c37f4a548
jb8429 <jackbicknell@gmail.com>
2024-09-10 17:44:02 +0100

    Added html

--- snap-go/main.go
+++ snap-go/main.go
@@ -6,11 +6,15 @@ import (
 )
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Snap the ge")
+	fmt.Fprintf(w, "Snap the game")
 }
 
 func main() {
+    fs := http.FileServer(http.Dir("./static")
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	http.HandleFunc("/", helloWorldHandler)
+	http.HandleFunc("/game", snapHandler)
 	fmt.Println("Server is running on port 8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
